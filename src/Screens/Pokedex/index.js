104c23972e51e1a4d8fe939203c6b52032aef4e4
Bruno Montich <montich.bruno@gmail.com>
2023-03-01 16:02:32 -0300

    fix

--- src/Screens/Pokedex/index.js
+++ src/Screens/Pokedex/index.js
@@ -8,82 +8,84 @@ import SearchBar from "../../components/SearchBar";
 import { useBackground } from "../../Context/background";
 
 function PokeInfo() {
-  const [pokes, setPokes] = useState([]);
-  const { pokelista, attPokelista } = useBackground();
+	const [pokes, setPokes] = useState([]);
+	const { pokelista, attPokelista } = useBackground();
 
-  useEffect(() => {
-    const fetchPokemons = async () => {
-      const { data } = await axios.get(
-        "https://pokeapi.co/api/v2/pokemon?limit=200"
-      );
-      setPokes(
-        data.results.map((poke, id) => ({
-          ...poke,
-          id: id + 1,
-        }))
-      );
-    };
-    fetchPokemons();
-  }, []);
+	useEffect(() => {
+		const fetchPokemons = async () => {
+			const { data } = await axios.get(
+				"https://pokeapi.co/api/v2/pokemon?limit=200"
+			);
+			setPokes(
+				data.results.map((poke, id) => ({
+					...poke,
+					id: id + 1,
+				}))
+			);
+		};
+		fetchPokemons();
+	}, []);
 
-  const imgResult = async () => {
-    if (Object.values(pokelista).length === 0) {
-      return;
-    }
-    const visiblePokes = Object.values(pokelista).filter(
-      (elm) => elm.isVisible
-    );
+	const imgResult = async () => {
+		if (Object.values(pokelista).length === 0) {
+			return;
+		}
+		const visiblePokes = Object.values(pokelista).filter(
+			(elm) => elm.isVisible
+		);
 
-    const randomIndex = Math.floor(Math.random() * visiblePokes.length);
-    const randomPoke = visiblePokes[randomIndex];
+		const randomIndex = Math.floor(Math.random() * visiblePokes.length);
+		const randomPoke = visiblePokes[randomIndex];
 
-    const result = randomPoke
-      ? await analyze(
-          `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomPoke?.id}.png`,
-          { ignore: ["rgb(255,255,255)", "rgb(0,0,0)"], scale: 1 }
-        )
-      : [
-          { color: "rgb(128, 155, 136)" },
-          { color: "rgb(128, 155, 136)" },
-          { color: "rgb(128, 155, 136)" },
-          { color: "rgb(128, 155, 136)" },
-        ];
-    console.log(randomPoke.name, result[0].color);
-    document.querySelector("body").style.backgroundColor = result[0].color;
-  };
+		const result = randomPoke
+			? await analyze(
+					`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomPoke?.id}.png`,
+					{ ignore: ["rgb(255,255,255)", "rgb(0,0,0)"], scale: 1 }
+			  )
+			: [
+					{ color: "rgb(128, 155, 136)" },
+					{ color: "rgb(128, 155, 136)" },
+					{ color: "rgb(128, 155, 136)" },
+					{ color: "rgb(128, 155, 136)" },
+			  ];
 
-  useEffect(() => {
-    imgResult();
-  }, [attPokelista]);
+		document.querySelector("body").style.backgroundColor = result[0].color;
+	};
 
-  const [search, setSearch] = useState("");
+	useEffect(() => {
+		imgResult();
+	}, [attPokelista]);
 
-  return (
-    <>
-      {pokes && (
-        <>
-          <SearchBar setSearch={setSearch} />
-          <div className="pokemon-container">
-            <div className="pokemon-list-container">
-              {pokes
-                .filter((val) => {
-                  if (search === "") {
-                    return val;
-                  } else if (
-                    val.name.toLowerCase().includes(search.toLowerCase())
-                  ) {
-                    return val;
-                  }
-                })
-                .map((poke) => (
-                  <Pokemon name={poke.name} id={poke.id} />
-                ))}
-            </div>
-          </div>
-        </>
-      )}
-    </>
-  );
+	const [search, setSearch] = useState("");
+
+	return (
+		<>
+			{pokes && (
+				<>
+					<SearchBar setSearch={setSearch} />
+					<div className="pokemon-container">
+						<div className="pokemon-list-container">
+							{pokes
+								.filter((val) => {
+									if (search === "") {
+										return val;
+									} else if (
+										val.name
+											.toLowerCase()
+											.includes(search.toLowerCase())
+									) {
+										return val;
+									}
+								})
+								.map((poke) => (
+									<Pokemon name={poke.name} id={poke.id} />
+								))}
+						</div>
+					</div>
+				</>
+			)}
+		</>
+	);
 }
 
 export default PokeInfo;
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -7,51 +7,51 @@ import { useBackground } from "../Context/background";
 import "./Pokemon.css";
 
 const Pokemon = ({ name, id }) => {
-  const [isVisible, setIsVisible] = useState(false);
-  const { pokelista, setPokelista, attPokelista, setAttPokelista } =
-    useBackground();
+	const [isVisible, setIsVisible] = useState(false);
+	const { pokelista, setPokelista, attPokelista, setAttPokelista } =
+		useBackground();
 
-  const reference = useRef();
+	const reference = useRef();
 
-  useEffect(() => {
-    let options = {
-      rootMargin: "0px",
-    };
+	useEffect(() => {
+		let options = {
+			rootMargin: "0px",
+		};
 
-    let observer = new IntersectionObserver((e) => {
-      setIsVisible(e[0].isIntersecting);
+		let observer = new IntersectionObserver((e) => {
+			setIsVisible(e[0].isIntersecting);
 
-      pokelista[name] = { name, id, isVisible: e[0].isIntersecting };
-      setPokelista(pokelista);
-      setAttPokelista(Math.random() * 1000);
-    }, options);
+			pokelista[name] = { name, id, isVisible: e[0].isIntersecting };
+			setPokelista(pokelista);
+			setAttPokelista(Math.random() * 1000);
+		}, options);
 
-    observer.observe(reference.current);
-  }, []);
+		observer.observe(reference.current);
+	}, []);
 
-  return (
-    <div className="pokemon-list">
-      <span>{isVisible.toString()}</span>
-      <span
-        style={{
-          color: "white",
-          fontWeight: "bold",
-          fontFamily: "Montserrat",
-        }}
-      >
-        {name.substring(0, 1).toUpperCase() + name.substring(1)}
-      </span>
+	return (
+		<div className="pokemon-list">
+			{/* <span>{isVisible.toString()}</span> */}
+			<span
+				style={{
+					color: "white",
+					fontWeight: "bold",
+					fontFamily: "Montserrat",
+				}}
+			>
+				{name.substring(0, 1).toUpperCase() + name.substring(1)}
+			</span>
 
-      <img
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
-        className="pokemon-img"
-        ref={reference}
-      />
-      <Link to={`/info/${id}`} className="pokemon-button">
-        Info
-      </Link>
-    </div>
-  );
+			<img
+				src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+				className="pokemon-img"
+				ref={reference}
+			/>
+			<Link to={`/info/${id}`} className="pokemon-button">
+				Info
+			</Link>
+		</div>
+	);
 };
 
 export default Pokemon;
